1227bb4cfd100266debcdd9833e113e45932ec92
Paul Tavitian <paul.tavitian@icloud.com>
2025-12-09 14:53:35 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,14 +10,13 @@ let package = Package(
         .library(
             name: "libamiitool",
             targets: ["libamiitool"]
-        ),
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "libamiitool"
-        ),
-        
+        )
     ]
 )
